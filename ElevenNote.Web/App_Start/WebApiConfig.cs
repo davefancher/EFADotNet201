09ed380e909b3dba483ba82d9b34e9b1d6b35685
Dave Fancher <dave@davefancher.com>
2016-04-10 17:31:01 -0400

    Connected note index page to WebAPI for toggling stars

---
+++ ElevenNote.Web/App_Start/WebApiConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace ElevenNote.Web
+{
+    public static class WebApiConfig
+    {
+        public static void Register()
+        {
+            GlobalConfiguration.Configure(x => x.MapHttpAttributeRoutes());
+        }
+    }
+}
