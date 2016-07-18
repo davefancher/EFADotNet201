1aceab775fff0b4522df11e7a1b1c671125fb595
Dave Fancher <dave@davefancher.com>
2016-07-17 22:58:18 -0400

    Imported GuessingGame code

---
+++ GuessingGame/GuessingGame/Controllers/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GuessingGame.Models;
+
+namespace GuessingGame.Controllers
+{
+    public class GameController : Controller
+    {
+        public ActionResult Index()
+        {
+            Session["Answer"] = new Random().Next(1, 10);
+
+            return View();
+        }
+
+        private bool GuessWasCorrect(int guess) =>
+            guess == (int)Session["Answer"];
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(GameViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Win = GuessWasCorrect(vm.Guess);
+            }
+
+            return View(vm);
+        }
+    }
+}
---
+++ GuessingGame/GuessingGame/Models/GameViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GuessingGame.Models
+{
+    public class GameViewModel
+    {
+        [Required]
+        [MinLength(2, ErrorMessage = "Too short")]
+        [MaxLength(20, ErrorMessage = "Too long")]
+        [Display(Name = "Player Name")]
+        public string PlayerName { get; set; }
+
+        [Required(ErrorMessage = "Guess is required.")]
+        [Range(1, 10, ErrorMessage = "Guess must be between 1 and 10")]
+        public int Guess { get; set; }
+
+        public override string ToString() => $"{PlayerName} ({Guess})";
+    }
+}
